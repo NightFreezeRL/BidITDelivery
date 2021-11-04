c51e39a1c2a8698a4b3765e2ff8b119161cede62
NightFreezeRL <nightfreezers@gmail.com>
2021-11-04 23:23:29 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div>
-    <h1>Welcome to BidIT Delivery system, please login to see your package status!</h1>
+    <h1 style="text-align: center;">Welcome to BidIT Delivery system, please login to see your package status!</h1>
 </div>
 @endsection('content')
