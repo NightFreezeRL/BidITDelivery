fc81827aef92b9e2c0bb1966a555d6db50a97be9
rupertlv <rob4iks4@gmail.com>
2021-11-04 20:58:37 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,8 +52,8 @@
                                 </li>
                             @endif
                         @else
-                        <a class="dropdown-item" href="http://127.0.0.1/main" onclick="event.preventDefault();
-                                                     document.getElementById('login-form').submit();">
+                        <a class="dropdown-item" href="http://127.0.0.1:8000/main">
+                                                    
                                         Mainpage
                         </a>
                         <a class="dropdown-item" href="{{ route('logout') }}"
