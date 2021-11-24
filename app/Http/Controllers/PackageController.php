1479e895eab66f2b048c0a209af83bcef4669f75
Dan1ss29 <danielsdementjevs16@gmail.com>
2021-11-25 00:00:18 +0200

    RegisterController changes

---
+++ app/Http/Controllers/PackageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PackageController extends Controller
+{
+    public function generatePackageId($length=5)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
